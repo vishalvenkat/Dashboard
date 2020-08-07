5a21a78d6b1f4192972ae3ec5c0a4cabdeb40280
vishalvenkat <venvis1998@gmail.com>
2020-08-07 17:44:44 +0530

    UI fix

--- src/app/Modules/material/material.module.ts
+++ src/app/Modules/material/material.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatSelectModule } from "@angular/material/select";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatMenuModule } from "@angular/material/menu";
 const materialModule = [
   MatIconModule,
   MatButtonModule,
   MatSelectModule,
-  MatSlideToggleModule,
   MatDialogModule,
   MatMenuModule,
 ];
